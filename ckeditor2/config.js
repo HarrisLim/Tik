e64f2b136db77c59bc6e03d304d8c5aa71d259d8
Harris Lim <harris19921204@gmail.com>
2017-11-07 19:40:24 +0900

    done image with  ckeditor

--- ckeditor2/config.js
+++ ckeditor2/config.js
@@ -7,4 +7,14 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.toolbar =[
+        ['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','Undo','Redo','SelectAll','RemoveFormat'],
+        '/',
+        ['Bold','Italic','Underline','Strike', 'Subscript','Superscript'],
+        ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+        ['NumberedList','BulletedList','Outdent','Indent','Blockquote'],
+        ['Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+        ['Styles','Format','Font','FontSize','TextColor','BGColor','Maximize', 'ShowBlocks']
+    ]
+    config.enterMode = CKEDITOR.ENTER_BR;
 };
